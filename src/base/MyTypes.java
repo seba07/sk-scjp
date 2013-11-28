ed630ea52ce080d6b76d7b9ee035c262456bf49e
seba07 <sebastian.karsznia@gmail.com>
2013-11-28 09:41:57 +0100

    Update MyTypes.java
    MyTypes with more description.

--- src/base/MyTypes.java
+++ src/base/MyTypes.java
@@ -26,17 +26,23 @@ public class MyTypes
 
 	public void testTypes() {
 
-		byte myByte = 0; // 8-bit
-
-		short myShort = 0; // 16-bit
-
-		char myChar = '0'; // 16-bit
-
-		int myInt = 0; // 32-bit
-
-		long myLon = 0; // 64-bit
-
-		double myDouble = 0; // 64-bit
+	        byte myByte = 0; // 8-bit
+	                         // -128 - 127
+	
+	        short myShort = 0; // 16-bit
+	                           // -32,768 - 32,767
+	        char myChar = '\u0000'; // 16-bit
+	        // '\u0000' (or 0) - '\uffff' (or 65,535) - Unicode characters
+	
+	        int myInt = 0;          // 32-bit
+	        // -2^31 - 2^31-1
+	        float myFloat = 0.0f;   // 32-bit
+	
+	        long myLong = 0L;        // 64-bit
+	        // -2^63 - 2^63-1
+	        double myDouble = 0.0d;    // 64-bit
+	
+	        boolean myBoolean = false;  // true/false
 
 	}
 
